0e8171f68a0776d637ef62709aeeac6106a4c920
andiemkhize <andiemkhize16@gmail>
2023-02-16 05:18:05 -0800

    Unix

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio>
+#include <unistf.h>
+/**
+ * main - a c program that prints a line to the standard error
+ * Reurn 1
+*/
+int main(void)
+{
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	return (1);
+}
