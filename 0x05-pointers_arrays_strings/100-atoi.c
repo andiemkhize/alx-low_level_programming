f12a2fc37a52386dae862d44b6ac2df7931785e3
andiemkhize <andiemkhize16@gmail>
2023-02-28 04:33:00 -0800

    100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * _atoi - function that convert a string to an integer
+ * atoi - function that convert a string to an integer
  * @s: string to convert
  * Return: int
  */
