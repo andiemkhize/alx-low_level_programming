acc40e2fb9d060e1540556790dd33113e13afbbc
andiemkhize <andiemkhize16@gmail>
2023-02-28 05:11:18 -0800

    100-atoi.c100-atoi.c100-atoi.c100-atoi.c100-atoi.c100-atoi.c100-atoi.c100-atoi.c100-atoi.c100-atoi.c100-atoi.c100-atoi.c100-atoi.c100-atoi.c100-atoi.c100-atoi.c100-atoi.c100-atoi.c100-atoi.c100-atoi.c100-atoi.c100-atoi.c100-atoi.c100-atoi.c100-atoi.c100-atoi.c100-atoi.c100-atoi.c100-atoi.c100-atoi.c100-atoi.c100-atoi.c100-atoi.c100-atoi.c100-atoi.c100-atoi.c100-atoi.c100-atoi.c100-atoi.c100-atoi.c100-atoi.c100-atoi.c100-atoi.c100-atoi.c100-atoi.c100-atoi.c100-atoi.c100-atoi.c100-atoi.c100-atoi.c100-atoi.c100-atoi.c100-atoi.c100-atoi.c100-atoi.c100-atoi.c100-atoi.c100-atoi.c100-atoi.c100-atoi.c100-atoi.c100-atoi.c100-atoi.c100-atoi.c100-atoi.c100-atoi.c100-atoi.c100-atoi.c100-atoi.c100-atoi.c100-atoi.c100-atoi.c100-atoi.c100-atoi.c100-atoi.c100-atoi.c100-atoi.c100-atoi.c100-atoi.c100-atoi.c100-atoi.c100-atoi.c100-atoi.c100-atoi.c100-atoi.c100-atoi.c100-atoi.c100-atoi.c100-atoi.c100-atoi.c100-atoi.c100-atoi.c100-atoi.c100-atoi.c100-atoi.c100-atoi.c100-atoi.c100-atoi.c100-atoi.c100-atoi.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -5,15 +5,5 @@
  * Return:  Always 0 (Success)
  */
 
-int main(void)
-{
-	char c;
-	c = 'a';
-	while 
-		(c <= 'z') {
-			putchar(c);
-			c++;
-		}
-		putchar('\n');
-	return (0);	
-} 
+int main(void) 
+{ 	
