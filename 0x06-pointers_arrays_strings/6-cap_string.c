4e9bc86cd3cb4ac0be57c22b27c2c022f4b39036
andiemkhize <andiemkhize16@gmail>
2023-03-02 03:56:39 -0800

    task 7

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * cap_string - capitalizes all words of a string
+ * @str: the string to be capitalized
+ *
+ * Return: a pointer to the charged string
+ */
+
+char *string_toupper(char *)
+{
+	int index = 0;
+
+	while (str[index])
+	{
+		while (!(str[index] >= 'a' && str[index] <= 'z'))
+			index++;
+
+		if (str[index - 1] == ' ' ||
+
+		tr[index	
