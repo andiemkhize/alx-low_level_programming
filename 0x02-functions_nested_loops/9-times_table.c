c26dea4f611544ff2c2c9d740bb47202cac0a02d
andiemkhize <andiemkhize16@gmail>
2023-03-03 07:46:16 -0800

    task 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -10,11 +10,24 @@ void times_table(void)
 {
 	int a;
 	int b;
+	int x;
+	int mul;
+	int chk;
 
 	for (a = 0; a <= 9; a++)
 		{
-			for (b = 0; b <= 9; b++)
-				printf("%d", a * b);
+			for (b = 0; x = 1; b <= 9; x++; b++)
+			{
+				mul = a * b;
+				chk = a * x;
+
+				if (chk > 9 && b != 9)
+				printf("%d", mul);
+				else if (b == 9)
+					printf("%d", mul);
+				else
+					printf("%d", mul);
+			}
 
 			putchar('\n');
 		}
