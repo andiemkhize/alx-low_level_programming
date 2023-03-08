045b3339c88e509cdb9df77557a865aae2261a1b
andiemkhize <andiemkhize16@gmail>
2023-03-08 01:02:52 -0800

    my task

---
+++ 0x02-functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_ numbers - print the numbers since 0 to 9
+ * Return: the numbers sinc e0 up to 9
+ */
+
+void print_numbers(void)
+{
+	int x;
+
+	for ( x = 0; x <= 9; x++)
+	{
+	_putchar(x + '0');
+	}
+        _putchar('\n');
+}	
---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - prints a Fizz buzz program 
+ * Return: 0
+ */
+
+int main(void)
+{
+	int x;
+
+	i
