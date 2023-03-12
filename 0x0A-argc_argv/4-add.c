64888b288f18e578bd259a1e93bd9fd86a4da528
andiemkhize <andiemkhize16@gmail>
2023-03-12 00:18:54 -0800

    100-chabge.c

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+/**
+ * check_num - check - string there are digit
+ * @str: array str
+ * Return: Always 0 (success)
+ */
+
+int check_num(char *str)
+{
+	unsigned int count;
+
+	count = 0;
+	while (count < strlen(str))
+	{
+		if (!isdigit(str[count])
+
+				{
+
+
