2bdf6438ed56f48869bd9fda06db71623d30f9c7
andiemkhize <andiemkhize16@gmail>
2023-03-24 16:21:19 -0700

    my header files

---
+++ 0x10-variadic_functions/_putchar.c
@@ -0,0 +1,13 @@
+#include "variadic_functions.h"
+#include <unistd.h>
+/**
+ * _putchar - writes the character c to stdout
+ * @c: the char to print
+ * Return: On success 1,
+ * On error, -1 is returned
+ */
+
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
