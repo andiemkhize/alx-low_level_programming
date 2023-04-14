9d37fb13b1f87d12cbc617d25e63f582b71f200f
andiemkhize <andiemkhize16@gmail.com>
2023-04-14 12:00:58 -0700

    1-create_file.c corrected

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -13,9 +13,10 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
+
 	if (text_context != NULL)
 	{
-		for (len = 0; text_content[len])
+		for (len = 0; text_content[len];)
 			len++;
 	}
 
