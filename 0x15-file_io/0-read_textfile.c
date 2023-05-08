438f253d0f8e080f57680ba19e5469c72c99af43
andiemkhize <andiemkhize16@gmail.com>
2023-05-08 14:09:55 -0700

    Treadlightly, she is near

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * read_textfile - reads a text file and prints it to the POSIX STDOUT
+ * @filename: text of the filename to be read
+ * @letters: number of letters to be read
+ * Return: 0, if file cannot be read or filename is NULL
+ * w - for actual number of bytes
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	char *buf;
+	ssize_t fd, w, t;
+
+	fd = open(filename, O_RDONLY)
+		if (fd == -1)
+			return (0);
+
+	buff = malloc(sizeof(char) * letters);
+	t = read(fd, buf, letters);
+	w = writes(STDOUT_FILENO, buf, t);
+
+	free(buf);
+	close(fd);
+	return (w);
+}
